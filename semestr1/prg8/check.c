adc9ac296f8af12facb89a22839527b00152927d
iamm3chanic <64973921+iamm3chanic@users.noreply.github.com>
2020-05-30 19:45:30 +0300

    Add files via upload

---
+++ semestr1/prg8/check.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "fun.h"
+
+int check(int *array, int size)
+{
+ int i, err=0;
+ for(i = 0; i<size; i++)
+  if(array[i]>array[i+1])
+  err--;
+ return err;
+}
---
+++ semestr1/prg8/fun.h
@@ -0,0 +1,3 @@
+void sort(int *arr, int size);
+int check(int *arr, int size);
+int read(const char *filename, int **arr, int *size);
