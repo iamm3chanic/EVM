ae7a4212806efa0cdfdc7483bd0c0e0d5381de12
Mechanic <64973921+iamm3chanic@users.noreply.github.com>
2021-08-03 00:03:40 +0300

    add task15

---
+++ semestr1/Autozach/solutions/Normasummakvadratovstolbca+++.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+    FILE *in,*out;
+    int N,M,i,j,k,l;
+    double normk=0,norml=0,flag=0;
+    double **a,*b;
+
+    in=fopen("input.txt","r");
+    if(in==NULL)
+    {
+        return(-1);
+    }
+
+    out=fopen("output.txt","w");
+    if(out==NULL)
+    {
+        return(-1);
+    }
+
+    if(fscanf(in,"%d%d",&N,&M)!=2)
+    {
+        return(-1);
+    }
+
+    if(N<=0 || M<=0)
+    {
+        return(-1);
+    }
+
+    a=(double**)malloc(N*sizeof(double*));
+    if(a==NULL)
+    {
+        return(-1);
+    }
+
+    for(i=0;i<N;i++)
+    {
+        a[i]=(double*)malloc(M*sizeof(double));
+        if(a[i]==NULL)
+        {
+            return(-1);
+        }
+    }
+
+    for(i=0;i<N;i++)
+    {
+        for (j=0;j<M;j++)
+        {
+            if(fscanf(in,"%lf",&a[i][j])!=1)
+            {
+                return(-1);
+            }
+        }
+    }
+
+    for(k=0;k<M;k++)
+    {
+        for(l=1;l<M-k;l++)
+        {
+            for(i=0;i<N;i++)
+            {
+                normk=normk+a[i][k]*a[i][k];
+            }
+            for(i=0;i<N;i++)
+            {
+                norml=norml+a[i][k+l]*a[i][k+l];
+            }
+            if(normk>norml)
+            {
+                for(i=0;i<N;i++)
+                {
+                    flag=a[i][k];
+                    a[i][k]=a[i][k+l];
+                    a[i][k+l]=flag;
+                }
+            }
+            normk=0;
+            norml=0;
+        }
+    }
+
+    fprintf(out,"%d %d\n",N,M);
+
+    for(i=0;i<N;i++)
+    {
+        for (j=0;j<M;j++)
+        {
+            fprintf(out,"%lf   ",a[i][j]);
+        }
+        fprintf(out,"\n");
+    }
+
+    b=(double*)malloc(N*sizeof(double));
+    if(b==NULL)
+    {
+        return(-1);
+    }
+
+    fprintf(out,"\n");
+
+    for(i=0;i<N;i++)
+    {
+        b[i]=0;
+        for(j=0;j<M;j++)
+        {
+            b[i]=b[i]+a[i][j]*a[N-1][j];
+        }
+        fprintf(out,"%lf  ",b[i]);
+    }
+
+    for(i=0;i<N;i++)
+    {
+        free(a[i]);
+    }
+    free(a);
+    free(b);
+
+    return 0;
+}
